ee3205558bc0b6490daabb68c2968f81b1cb24b6
NateNet59 <98017840+NateNet59@users.noreply.github.com>
2022-03-25 21:22:47 -0600

    Add files via upload

--- pageObjects.ts
+++ pageObjects.ts
@@ -8,18 +8,28 @@ export class Scheels extends BasePage {
     searchButton: By = By.xpath('//button[@id="header-search-btn"]')
     magBlack: By = By.xpath('(//a[@class="swatchanchor mx-1 my-1"])[1]')
     addPickup: By = By.xpath('(//button[@data-href="/on/demandware.store/Sites-scheels-Site/en_US/ItemAdded-ItemAdded?pid=87375000617"])[2]')
-    viewCart: By = By.xpath('//a[@button="button is-primary mini-cart-link-cart is-fullwidth"]')
+    viewCart: By = By.xpath('//a[text()="View Cart & Checkout"]')
     removeCart: By = By.xpath('//button[@class="button-text"]')
-    addQuantity: By = By.xpath('//input[@aria-label="Increase Quantity by 1"]')
+    addQuantity: By = By.xpath('//input[@id="Quantity-Web"]')
     myStore: By = By.xpath('(//button[@class="dropbtn"])[1]')
     changeStore: By = By.xpath('(//a[@class="select-preferred-store"])[1]')
-    overlandPark: By = By.xpath('(//a[@class="set-preferred-store-link button is-primary"]([19]')
+    overlandPark: By = By.xpath('(//a[@class="set-preferred-store-link button is-primary"])[19]')
     findOther: By = By.xpath('//a[@class="button is-primary search-preferred-stores mb-5"]')
     storeDetail: By = By.xpath('(//a[@href="/store?StoreID=088"])[1]')
     zipBox: By = By.xpath('//input[@class="input-text  required"]')
     zipSearch: By = By.xpath('(//button[@type="submit"])[3]')
     eauClaire: By = By.xpath('(//a[@class="set-preferred-store-link button is-primary"])[1]')
-
+    closePopup: By = By.xpath('//button[@id="closeIconContainer"]')
+    noThanks: By = By.xpath('//button[@id="dismissbutton2"]')
+    closeCookies: By = By.xpath('//button[@id="cookiesClose"]')
+    searchItem: By = By.xpath('(//img[@class="product-tile-image lozad fade"])[1]')
+    signDrop: By = By.xpath('(//button[@aria-expanded="false"])[4]')
+    accountMenu: By = By.xpath('//a[@id="header-menu-log-in"]')
+    loginEmail: By = By.xpath('//input[@class="input-text email  required"]')
+    passWord: By = By.xpath('//input[@class="input-text  required"]')
+    signButton: By = By.xpath('//button[@class="g-recaptcha"]')
+    logout: By = By.xpath('//a[@title="Logout"]')
+    
     
 
     
--- switchLocation.test.ts
+++ switchLocation.test.ts
@@ -4,18 +4,23 @@ const scheels = new Scheels;
 
 test("add to cart", async () => {
     await scheels.navigate()
+    await scheels.driver.manage().window().maximize()
+    await scheels.driver.sleep(10000)
+    //await scheels.click(scheels.closePopup)
+    await scheels.click(scheels.closeCookies)
     await scheels.click(scheels.myStore)
     await scheels.click(scheels.changeStore)
     await scheels.click(scheels.overlandPark)
+    await scheels.driver.sleep(5000)
     await scheels.click(scheels.myStore)
     await scheels.click(scheels.storeDetail)
+    await scheels.driver.sleep(5000)
     await scheels.click(scheels.myStore)
     await scheels.click(scheels.changeStore)
     await scheels.click(scheels.findOther)
     await scheels.setInput(scheels.zipBox, "54701")
     await scheels.click(scheels.zipSearch)
     await scheels.click(scheels.eauClaire)
-
     await scheels.driver.quit()
 
 })
